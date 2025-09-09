47e1763565f4312dfaedd0039ba16bd9a3ae2bd1
Apeksha-12ap <sidddaundkar@gmail.com>
2025-09-09 16:42:08 +0530

    Terraform Code

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "ap-south-1"
+  profile = "terraform"
+}
+
+resource "aws_instance" "jenkins_server" {
+
+  ami               = "ami-0038df39db13a87e2"
+  instance_type     = "t2.medium"
+  availability_zone = "ap-south-1a"
+  key_name          = "Mumbaikey"
+
+  tags = {
+    Name = "Jenkins_Pipeline_Server"
+  }
+
+}
